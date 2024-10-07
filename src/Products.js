019b265b5f07740d37297d35b4feb95e9ea119c7
rajesh thiruppathi <info@iepsolutions.in>
2024-10-08 02:08:43 +0530

    Updated Products.js with dynamic image paths

--- src/Products.js
+++ src/Products.js
@@ -5,18 +5,18 @@ function Products() {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        // Sample product data with relative image paths
+        // Sample product data with dynamic PUBLIC_URL for images
         setProducts([
-            { _id: '1', name: 'Product 1', price: 29.99, image: '/images/product1.jpg' },
-            { _id: '2', name: 'Product 2', price: 39.99, image: '/images/product2.jpg' },
-            { _id: '3', name: 'Product 3', price: 49.99, image: '/images/product3.jpg' },
-            { _id: '4', name: 'Product 4', price: 19.99, image: '/images/product4.jpg' },
-            { _id: '5', name: 'Product 5', price: 59.99, image: '/images/product5.jpg' },
-            { _id: '6', name: 'Product 6', price: 79.99, image: '/images/product6.jpg' },
-            { _id: '7', name: 'Product 7', price: 89.99, image: '/images/product7.jpg' },
-            { _id: '8', name: 'Product 8', price: 99.99, image: '/images/product8.jpg' },
-            { _id: '9', name: 'Product 9', price: 109.99, image: '/images/product9.jpg' },
-            { _id: '10', name: 'Product 10', price: 119.99, image: '/images/product10.jpg' },
+            { _id: '1', name: 'Product 1', price: 29.99, image: `${process.env.PUBLIC_URL}/images/product1.jpg` },
+            { _id: '2', name: 'Product 2', price: 39.99, image: `${process.env.PUBLIC_URL}/images/product2.jpg` },
+            { _id: '3', name: 'Product 3', price: 49.99, image: `${process.env.PUBLIC_URL}/images/product3.jpg` },
+            { _id: '4', name: 'Product 4', price: 19.99, image: `${process.env.PUBLIC_URL}/images/product4.jpg` },
+            { _id: '5', name: 'Product 5', price: 59.99, image: `${process.env.PUBLIC_URL}/images/product5.jpg` },
+            { _id: '6', name: 'Product 6', price: 79.99, image: `${process.env.PUBLIC_URL}/images/product6.jpg` },
+            { _id: '7', name: 'Product 7', price: 89.99, image: `${process.env.PUBLIC_URL}/images/product7.jpg` },
+            { _id: '8', name: 'Product 8', price: 99.99, image: `${process.env.PUBLIC_URL}/images/product8.jpg` },
+            { _id: '9', name: 'Product 9', price: 109.99, image: `${process.env.PUBLIC_URL}/images/product9.jpg` },
+            { _id: '10', name: 'Product 10', price: 119.99, image: `${process.env.PUBLIC_URL}/images/product10.jpg` },
         ]);
     }, []);
 
